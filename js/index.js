de782faa8663d791b4a5fda810b2bbdcf632fa85
Juan Esteban Londoño Gomez <72523178+Estebanxz11@users.noreply.github.com>
2024-03-23 10:49:10 -0500

    ajuste formulario de reserva

--- js/index.js
+++ js/index.js
@@ -31,3 +31,33 @@ document.addEventListener('DOMContentLoaded', function() {
     document.querySelector('.carousel-prev').addEventListener('click', prevSlide);
     document.querySelector('.carousel-next').addEventListener('click', nextSlide);
 });
+
+
+ // Función para generar intervalos de hora
+ function generateHourOptions() {
+    var select = document.getElementById("hora");
+    for (var i = 0; i < 24; i++) {
+        var hour = ("0" + i).slice(-2) + ":00"; // Añade un cero delante de las horas menores de 10
+        var option = document.createElement("option");
+        option.text = hour;
+        option.value = hour;
+        select.add(option);
+    }
+}
+
+// Función para obtener la fecha actual y establecerla en el campo de fecha
+function setCurrentDate() {
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0'); // Enero es 0!
+    var yyyy = today.getFullYear();
+
+    today = yyyy + '-' + mm + '-' + dd;
+    document.getElementById("fecha").value = today;
+}
+
+// Llamar a las funciones al cargar la página
+window.onload = function () {
+    generateHourOptions();
+    setCurrentDate();
+};
